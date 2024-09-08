588b429a6e6e447a0c07b5d71c518bc7ab5f012f
Alex <psyab15@nottingham.ac.uk>
2024-09-08 23:11:01 +0100

    base rock paper scissors working

--- RockPaperScissors/Option.cs
+++ RockPaperScissors/Option.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,33 +15,51 @@ namespace RockPaperScissors
         private int compInt;
 
         public string getStr() { return optStr; }
+        public int getInt() { return compInt; }
 
         public Option( char type ) 
         {
             switch (type)
             {
                 case 'r':
-                    optStr = "rock";
+                    optStr = "Rock";
                     compInt = 0;
                     break;
                 case 'p':
-                    optStr = "paper";
+                    optStr = "Paper";
                     compInt = 1;
                     break;
                 case 's':
-                    optStr = "scissors";
+                    optStr = "Scissors";
                     compInt = 2;
                     break;
             }
         }
+        public Option(int type)
+        {
+            switch (type)
+            {
+                case 0:
+                    optStr = "Rock";
+                    break;
+                case 1:
+                    optStr = "Paper";
+                    break;
+                case 2:
+                    optStr = "Scissors";
+                    break;
+            }
+            compInt = type;
+        }
 
-        public static int compareOption(Option op1, Option op2) { 
-            if(op1.compInt == (op2.compInt + 1) % 3)
+        public static int compareOption(Option op1, Option op2) {
+            if (op1.compInt == (op2.compInt + 1) % 3)
             {
                 // if modulo 3, op1 is one more than it wins and vice versa
                 return 1;
             }
-            else if( op1.compInt == (op2.compInt - 1) % 3)
+            // modulus in c# is remainder so +2 works when -1 doesnt as it means the same but does not go negative
+            else if( op1.compInt == (op2.compInt + 2) % 3)
             {
                 return 2;
             }
--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -6,7 +6,46 @@ namespace RockPaperScissors
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                baseRPS();
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+
+        static void baseRPS()
+        {
+            Random rand = new Random();
+
+            Console.WriteLine("Enter your option");
+            char choice = Console.ReadLine().ToCharArray()[0];
+            Option player = new Option(choice);
+
+            Option computer = new Option(rand.Next(3));
+
+            int outcome = Option.compareOption(player, computer);
+
+
+
+
+            Console.WriteLine("You played:          " + player.getStr());
+            Console.WriteLine("And computer player: " + computer.getStr());
+
+            switch (outcome)
+            {
+                case 0:
+                    Console.WriteLine("So it's a Draw");
+                    break;
+                case 1:
+                    Console.WriteLine("You Win!");
+                    break;
+                case 2:
+                    Console.WriteLine("You Lose :(");
+                    break;
+            }
+            Console.WriteLine("Press Enter to play again");
+
         }
     }
 }
