d733d675120d82889acae2b7f4c7c2edcf5ddd96
Alex <psyab15@nottingham.ac.uk>
2024-09-09 00:40:35 +0100

    lizard spock mode added

---
+++ RockPaperScissors/OptionRPSLS.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    internal class OptionRPSLS
+    {
+        //Stores the name of the option for use
+        private string optStr;
+        // Int is better for comparisons
+        private int compInt;
+
+        public string getStr() { return optStr; }
+        public int getInt() { return compInt; }
+
+        public OptionRPSLS(char type)
+        {
+            switch (type)
+            {
+                case 'r':
+                    optStr = "Rock";
+                    compInt = 0;
+                    break;
+                case 'p':
+                    optStr = "Paper";
+                    compInt = 1;
+                    break;
+                case 's':
+                    optStr = "Scissors";
+                    compInt = 2;
+                    break;
+                case 'v':
+                    optStr = "Spock";
+                    compInt = 3;
+                    break;
+                case 'l':
+                    optStr = "Lizard";
+                    compInt = 4;
+                    break;
+                default:
+                    throw new ArgumentException("Invalid constructor input");
+            }
+        }
+        public OptionRPSLS(int type)
+        {
+            switch (type)
+            {
+                case 0:
+                    optStr = "Rock";
+                    break;
+                case 1:
+                    optStr = "Paper";
+                    break;
+                case 2:
+                    optStr = "Scissors";
+                    break;
+                case 3:
+                    optStr = "Spock";
+                    break;
+                case 4:
+                    optStr = "Lizard";
+                    break;
+                default:
+                    throw new ArgumentException("Invalid constructor input");
+            }
+            compInt = type;
+        }
+
+        public static int compareOption(OptionRPSLS op1, OptionRPSLS op2)
+        {
+            Console.WriteLine(op1.compInt);
+            Console.WriteLine(op2.compInt);
+            if (op1.compInt == (op2.compInt + 1) % 5
+               || op1.compInt == (op2.compInt + 3) % 5)
+            {
+                Console.WriteLine("h1");
+                return 1;
+            }
+            // modulus in c# is remainder so +4 works when -1 doesnt as it means the same but does not go negative
+            else if (op1.compInt == (op2.compInt + 4) % 5
+                || op1.compInt == (op2.compInt + 2) % 5)
+            {
+                Console.WriteLine("h2");
+                return 2;
+            }
+            //else if (op1.compInt == (op2.compInt + 2) % 5)
+            //{
+            //    Console.WriteLine("h3");
+            //    return 1;
+            //}
+            //else if (op1.compInt == (op2.compInt + 3) % 5)
+            //{
+            //    Console.WriteLine("h4");
+            //    return 2;
+            //}
+            else
+            {
+                // 0 represents a draw
+                return 0;
+            }
+        }
+    }
+}
--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -12,15 +12,18 @@ namespace RockPaperScissors
             while (true)
             {
                 Console.WriteLine(previous);
-                previous = baseRPS(input, previous);
+                previous = rpsls(input, previous);
             }
         }
 
-        static int baseRPS(int compChoice, int prev)
+        static int rps(int compChoice, int prev)
         {
             Random rand = new Random();
 
-            Console.WriteLine("Enter your option (r/p/s)");
+            Console.WriteLine("Enter your option: ");
+            Console.WriteLine("r = rock");
+            Console.WriteLine("p = paper");
+            Console.WriteLine("s = scissors");
             char choice = Console.ReadLine().ToLower().ToCharArray()[0];
             Option player = new Option(choice);
 
@@ -33,7 +36,7 @@ namespace RockPaperScissors
             }
             else if (compChoice == 1)
             {
-                computer = previousChoice(prev, rand);
+                computer = previousChoiceRPS(prev, rand);
             }
 
             // Judge winner
@@ -41,7 +44,7 @@ namespace RockPaperScissors
 
 
             Console.WriteLine("You played:          " + player.getStr());
-            Console.WriteLine("And computer player: " + computer.getStr());
+            Console.WriteLine("The computer played: " + computer.getStr());
 
             switch (outcome)
             {
@@ -63,7 +66,61 @@ namespace RockPaperScissors
             return player.getInt();
         }
 
-        private static Option previousChoice(int prev, Random rand)
+
+        static int rpsls(int compChoice, int prev)
+        {
+            Random rand = new Random();
+
+            Console.WriteLine("Enter your option: ");
+            Console.WriteLine("r = rock");
+            Console.WriteLine("p = paper");
+            Console.WriteLine("s = scissors");
+            Console.WriteLine("l = lizard");
+            Console.WriteLine("v = spock");
+            char choice = Console.ReadLine().ToLower().ToCharArray()[0];
+            OptionRPSLS player = new OptionRPSLS(choice);
+
+            // Will get overwritten
+            OptionRPSLS computer = new OptionRPSLS(0);
+
+            if (compChoice == 0)
+            {
+                computer = new OptionRPSLS(rand.Next(5));
+            }
+            else if (compChoice == 1)
+            {
+                computer = previousChoiceRPSLS(prev, rand);
+            }
+
+            // Judge winner
+            int outcome = OptionRPSLS.compareOption(player, computer);
+
+
+            Console.WriteLine("You played:          " + player.getStr());
+            Console.WriteLine("The computer played: " + computer.getStr());
+
+            switch (outcome)
+            {
+                case 0:
+                    Console.WriteLine("So it's a Draw");
+                    break;
+                case 1:
+                    Console.WriteLine("You Win!");
+                    break;
+                case 2:
+                    Console.WriteLine("You Lose :(");
+                    break;
+            }
+            Console.WriteLine("Press Enter to play again");
+            Console.ReadLine();
+            Console.Clear();
+
+            // Return player choice for next round
+            return player.getInt();
+        }
+
+
+        private static Option previousChoiceRPS(int prev, Random rand)
         {
             Option output;
             if (prev == -1)
@@ -74,6 +131,21 @@ namespace RockPaperScissors
             else
             {
                 output = new Option(prev);
+            }
+            return output;
+        }
+
+        private static OptionRPSLS previousChoiceRPSLS(int prev, Random rand)
+        {
+            OptionRPSLS output;
+            if (prev == -1)
+            {
+                // Random if first rotation
+                output = new OptionRPSLS(rand.Next(5));
+            }
+            else
+            {
+                output = new OptionRPSLS(prev);
             }
             return output;
         }
